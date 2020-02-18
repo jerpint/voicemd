980e4b736bf9d05ef0c882dfb75345ba809a4496
Jeremy Pinto <jerpint@gmail.com>
2020-02-18 17:41:14 -0500

    Change classifier layer directly in densenet

--- voicemd/models/densenet.py
+++ voicemd/models/densenet.py
@@ -3,9 +3,7 @@ import torchvision.models as models
 
 def densenet121(hyper_params=None):
 
-    ### TODO, figure out if we want to pop the last layer
     densenet = models.densenet121(pretrained=True)
-    #  densenet._modules.pop('classifier')
-    densenet = torch.nn.Sequential(densenet, torch.nn.Linear(in_features=1000, out_features=1))
-
+    densenet.classifier = torch.nn.Linear(in_features=1024, out_features=512)
+    densenet = torch.nn.Sequential(densenet, torch.nn.Linear(in_features=512, out_features=1))
     return densenet

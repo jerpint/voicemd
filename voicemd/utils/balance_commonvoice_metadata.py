d8194f954398a635bdee570aa7ce80b42d8773f3
Jeremy Pinto <jerpint@gmail.com>
2020-09-30 17:26:13 -0400

    add splits

--- voicemd/utils/balance_commonvoice_metadata.py
+++ voicemd/utils/balance_commonvoice_metadata.py
@@ -1,28 +1,34 @@
 import pandas as pd
 
 
-def balance_and_filter_commonvoice_tsv(tsv):
+def balance_and_filter_commonvoice_tsv(tsv, split, seed=42):
     """
 
     We remove entries with no associated gender.
     There is a big bias towards males in twenties.
-    We sample and rebalance the dataset such that there are
+    We sample and rebalance the train dataset such that there are
     equal samples across males and females for all age categoires
     in valid_age_categories.
     """
 
     metadata = tsv.copy()
-    seed = 42
+    metadata.rename(columns={"path": "filename"}, inplace=True)
+
+    # show stats before rebalancing
+    print("Breakdown before rebalance: \n")
+    print_metadata_stats(metadata)
+
+    # do not rebalance dev and test
+    if split != "train":
+        print(split + " not rebalanced")
+        return metadata
+
     # final samples will be stored here
     male_metadata = pd.DataFrame(columns=metadata.columns)
     female_metadata = pd.DataFrame(columns=metadata.columns)
 
     # remove samples where gender is unidentified
     metadata = metadata[metadata["gender"].isin(["male", "female"])]
-
-    # show stats before rebalanceing
-    print("Breakdown before rebalance: \n")
-    print_metadata_stats(metadata)
 
     # keep only valid age categories
     valid_age_categories = ["twenties", "thirties", "fourties", "fifties", "sixties"]
@@ -57,7 +63,6 @@ def balance_and_filter_commonvoice_tsv(tsv):
 
 
 def print_metadata_stats(metadata):
-    print("Breakdown after processing \n")
     print("Gender breakdown: \n", metadata["gender"].value_counts())
     print(
         "Age breakdown by gender (male): \n",
@@ -70,11 +75,13 @@ def print_metadata_stats(metadata):
 
 
 if __name__ == "__main__":
-    common_voice_path = "/data/mozilla/"
-    tsv_fname = common_voice_path + "train.tsv"
+    commonvoice_path = "/data/mozilla/"
+    splits = ["train", "dev", "test"]
 
-    tsv = pd.read_csv(tsv_fname, sep="\t")
-    print("reading ", tsv_fname)
-
-    metadata = balance_and_filter_commonvoice_tsv(tsv)
-    metadata.rename(columns={"path": "filename"}, inplace=True)
+    for split in splits:
+        print(50 * "=")
+        tsv_fname = commonvoice_path + split + ".tsv"
+        tsv = pd.read_csv(tsv_fname, sep="\t")
+        print("reading ", tsv_fname)
+        metadata = balance_and_filter_commonvoice_tsv(tsv, split)
+        metadata.to_csv(commonvoice_path + "cv_" + split + "_metadata.csv")

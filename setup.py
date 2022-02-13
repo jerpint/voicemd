db9aee55cdecdfd8a93c6e995eeadda5ba83af8d
Jeremy Pinto <jerpint@gmail.com>
2022-02-12 19:40:40 -0500

    move requirements to dev

--- setup.py
+++ setup.py
@@ -2,16 +2,26 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='voicemd',
-    version='0.0.1',
-    packages=find_packages(include=['voicemd', 'voicemd.*']),
-    python_requires='>=3.6.9',
+    name="voicemd",
+    version="0.0.1",
+    packages=find_packages(include=["voicemd", "voicemd.*"]),
+    python_requires=">=3.6.9",
     install_requires=[
-        'flake8', 'tqdm', 'mlflow', 'orion', 'pyyaml', 'pytest',
-        'torch', 'torchaudio', 'librosa>=0.8'],
+        "pyyaml",
+        "torch",
+        "torchaudio",
+        "librosa>=0.8",
+    ],
+    extras_require={
+        'dev': [
+            "flake8",
+            "tqdm",
+            "mlflow",
+            "orion",
+            "pytest",
+        ]
+    },
     entry_points={
-        'console_scripts': [
-            'main=voicemd.main:main'
-        ],
-    }
+        "console_scripts": ["main=voicemd.main:main"],
+    },
 )

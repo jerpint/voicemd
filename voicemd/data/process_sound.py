d5f6e218c0ef1d7ed53854531b97f0f05314e633
Jeremy Pinto <jerpint@gmail.com>
2022-02-19 11:48:56 -0500

    Add functions to fetch files

--- voicemd/data/process_sound.py
+++ voicemd/data/process_sound.py
@@ -1,3 +1,7 @@
+import os
+import requests
+import time
+
 import torch
 import librosa
 import numpy as np
@@ -6,19 +10,27 @@ import torchaudio
 
 def compute_specgram(waveform, sr, spec_type, normalize):
 
-    if spec_type == 'librosa_melspec':
-        specgram = librosa.feature.melspectrogram(y=waveform, sr=sr, hop_length=512, win_length=512, fmax=8000, n_mels=80)
+    if spec_type == "librosa_melspec":
+        specgram = librosa.feature.melspectrogram(
+            y=waveform, sr=sr, hop_length=512, win_length=512, fmax=8000, n_mels=80
+        )
         specgram = librosa.power_to_db(specgram, ref=np.max)
         specgram = torch.tensor(specgram).unsqueeze(dim=0)
 
-    elif spec_type == 'pytorch_spec':
-        specgram = torchaudio.transforms.Spectrogram(n_fft=400, normalized=True)(waveform)
+    elif spec_type == "pytorch_spec":
+        specgram = torchaudio.transforms.Spectrogram(n_fft=400, normalized=True)(
+            waveform
+        )
 
-    elif spec_type == 'pytorch_melspec':
-        specgram = torchaudio.transforms.MelSpectrogram(sample_rate=sr, n_mels=40)(waveform)
+    elif spec_type == "pytorch_melspec":
+        specgram = torchaudio.transforms.MelSpectrogram(sample_rate=sr, n_mels=40)(
+            waveform
+        )
 
-    elif spec_type == 'pytorch_mfcc':
-        specgram = torchaudio.transforms.MFCC(sample_rate=sr, n_mfcc=40, log_mels=False, melkwargs={'n_fft': 800})(waveform)
+    elif spec_type == "pytorch_mfcc":
+        specgram = torchaudio.transforms.MFCC(
+            sample_rate=sr, n_mfcc=40, log_mels=False, melkwargs={"n_fft": 800}
+        )(waveform)
 
     else:
         raise ValueError("spec_type not defined")
@@ -26,7 +38,7 @@ def compute_specgram(waveform, sr, spec_type, normalize):
     if normalize:
         # Center values about 0 since
         # dbs range from -80 to 0
-        specgram = (specgram + 40)
+        specgram = specgram + 40
 
     return specgram
 
@@ -46,3 +58,23 @@ def load_waveform(fname):
         waveform = np.concatenate((waveform, waveform))
 
     return waveform, sr
+
+
+def save_sound_from_url(url: str, filename: str):
+    """Fetch a sound file from a url and save it to disk."""
+    with open(filename, 'wb') as file_:
+      file_.write(requests.get(url).content)
+
+
+def generate_filename_with_timestamp(tmpdir="/tmp"):
+    """Generate a filename from a timestamp in tmpdir."""
+    filename = "sound_" + time.strftime("%Y%m%d_%H%M%S") + ".wav"
+    return os.path.join(tmpdir, filename)
+
+
+def fetch_sound(url):
+    """Downlod and save sound locally."""
+    #  filename = generate_filename_with_timestamp()
+    filename = 'tmp_sound.wav'
+    save_sound_from_url(url, filename)
+    return filename

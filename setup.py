b71e3f19bf2f771b22d2192d929548316d55d695
Jeremy Pinto <jerpint@gmail.com>
2023-06-12 23:27:55 -0400

    add mlflow dependency

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ setup(
     packages=find_packages(include=['voicemd', 'voicemd.*']),
     python_requires='>=3.9',
     install_requires=[
+        'librosa==0.10.0.post2',
+        'mlflow==2.4.1',
+        'numpy==1.24.3'
         'torch==2.0.1',
         'torchaudio==2.0.2',
         'torchvision==0.15.2',
-        'librosa==0.10.0.post2',
-        'numpy==1.24.3'],
+    ],
     entry_points={
         'console_scripts': [
             'main=voicemd.main:main'
